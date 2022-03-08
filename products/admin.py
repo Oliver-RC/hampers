98e4950116d0aa7810fcaf825ea27eb6b662e6e4
Oliver-RC <oli_cummings9@hotmail.com>
2022-03-08 14:43:41 +0000

    amend admin for products app for better display of information

--- products/admin.py
+++ products/admin.py
@@ -3,6 +3,31 @@ from .models import Product, Category, Occasion
 
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Occasion)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'occasion',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class OccasionAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Occasion, OccasionAdmin)
