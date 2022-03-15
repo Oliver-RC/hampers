030336d06d5dae2019017d02be648e5d52eca1f6
Oliver-RC <oli_cummings9@hotmail.com>
2022-03-15 17:06:14 +0000

    updated bag context with contents visable

--- bag/views.py
+++ bag/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 
-# Create your views here.
 
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
 
 def add_to_bag(request, item_id):
     """ Add a set quantity of the selected product to the shopping bag """
@@ -20,5 +20,4 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
