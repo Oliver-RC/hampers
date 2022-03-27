ff712b5808dea922a700ab753002f4b77255bcf3
Oliver-RC <oli_cummings9@hotmail.com>
2022-03-27 11:55:17 +0000

    stripe webhooks working successfully amending webhook handler functionality

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -7,6 +7,14 @@ class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
 
+    def handle_event(self, event):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
     def handle_payment_intent_succeeded(self, event):
         """
         Handle the payment_intent.succeeded webhook from Stripe
