141db06ec8f452bf9d1a0b16bfa6d2a0b73f4c80
Oliver-RC <oli_cummings9@hotmail.com>
2022-04-01 19:35:28 +0000

    review form, admin and template started

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .widgets import CustomClearableFileInput
-from .models import Product, Category, Occasion
+from .models import Product, Category, Occasion, Reviews
 
 
 class ProductForm(forms.ModelForm):
@@ -22,3 +22,10 @@ class ProductForm(forms.ModelForm):
         self.fields['occasion'].choices = occasion_friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-secondary'
+
+
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Reviews
+        fields = ['subject', 'review', 'star_rating']
