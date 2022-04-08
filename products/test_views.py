15c144a4205a2f31ac383c3c05c0dde305d3d896
Oliver-RC <oli_cummings9@hotmail.com>
2022-04-08 08:51:38 +0000

    added tests for retrieving the home, product and bag pages

---
+++ products/test_views.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class TestViews(TestCase):
+
+    def test_get_products(self):
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/products.html')
